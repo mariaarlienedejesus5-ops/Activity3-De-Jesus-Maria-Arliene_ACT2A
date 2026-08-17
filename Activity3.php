cf8fca1bb09ded49170c006d2cc3532125a02eea
mariaarlienedejesus5-ops <mariaarlienedejesus5@gmail.com>
2026-08-17 20:25:10 +0800

    Add grade evaluation logic in Activity3.php

---
+++ Activity3.php
@@ -0,0 +1,30 @@
+<?php
+
+$grade = 49;
+
+if ($grade >= 90 && $grade <= 100) {
+    $letterGrade = "A";
+    $remarks = "Passed";
+} elseif ($grade >= 80 && $grade <= 89) {
+    $letterGrade = "B";
+    $remarks = "Passed";
+} elseif ($grade >= 70 && $grade <= 79) {
+    $letterGrade = "C";
+    $remarks = "Passed";
+} elseif ($grade >= 60 && $grade <= 69) {
+    $letterGrade = "D";
+    $remarks = "Failed";
+} elseif ($grade >= 0 && $grade <= 59) {
+    $letterGrade = "F";
+    $remarks = "Failed";
+} else {
+    $letterGrade = "Invalid";
+    $remarks = "Invalid grade";
+}
+
+echo "Grade Evaluation\n";
+echo "Numeric Grade: " . $grade . "\n";
+echo "Letter Grade: " . $letterGrade . "\n";
+echo "Remarks: " . $remarks;
+
+?>
